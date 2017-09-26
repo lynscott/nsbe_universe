ec0bb83768792816f6f8d9dda0109241eb847877
Lyn Scott <lennord@gmail.com>
2017-09-26 08:55:01 -0700

    Update: Refactor code and update. Seperated files for version 2

---
+++ NU_V2/database_setup.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+    points = Column(Integer)
+
+
+class Event(Base):
+    __tablename__ = 'event'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    points = Column(Integer)
+    address = Column(String(250), nullable=False)
+    details = Column(String(250), nullable=False)
+    picture = Column(String(250))
+    date = Column(Date, nullable=False)
+    url = Column(Text)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            'points': self.points,
+        }
+
+
+engine = create_engine('sqlite:///nsbeuniv.db')
+
+
+Base.metadata.create_all(engine)
---
+++ NU_V2/nsbeu.py
@@ -0,0 +1,76 @@
+from flask import (Flask, render_template, request, redirect,
+                    url_for, jsonify, flash)
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Catalog, CatalogItem, User
+from flask import session as login_session
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+from functools import wraps
+
+app = Flask(__name__)
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "nsbe universe"
+
+
+engine = create_engine('sqlite:///nsbeuniv.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# Create login decorator
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'username' not in login_session:
+            return redirect('/login')
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+#Helper functions
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).first()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).first()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).first()
+        return user.id
+    except:
+        return None
+
+
+@app.route('/')
+@app.route('/home/')
+def home():
+    current_user = getUserInfo(User.user_id)
+    return render_template('index.html', current_user=current_user)
+
+# Show all sauce catalogs
+@app.route('/events/')
+def showEvents():
+    events = session.query(Event).all()
+    creator = getUserInfo(Event.user_id)
+    # return "This page will show all my catalogs of various sauces"
+    return render_template('eventList.html', events=events, creator=creator)
