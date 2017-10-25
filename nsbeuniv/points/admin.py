559e5377872b790eff0b6d8e0ef73e44742dd6f3
Lyn Scott <lennord@gmail.com>
2017-10-24 17:35:54 -0700

    RM: Removed outdated version.

--- nsbeuniv/points/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-
-class EventInfoInline(admin.TabularInline):
-    model = models.Event
-
-admin.site.register(models.Event)
